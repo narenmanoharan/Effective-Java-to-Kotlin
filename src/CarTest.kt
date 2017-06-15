dbe193d16ce0104177caaf686fd0beb0f33d82d2
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-15 00:15:33 -0500

    Added Effective Kotlin Item 1 test

--- src/CarTest.kt
+++ src/CarTest.kt
@@ -1,10 +1,16 @@
-import org.junit.Before
-
-import org.junit.Assert.*
+import Brand.BMW
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Created by Naren on 6/14/17.
  */
 class CarTest {
-  @Before fun setUp() {}
+
+  val car : Car = Car.valueOf("M3", BMW, 260, 80, 160, 2, 14)
+
+  @Test fun checkCarValue() {
+    assertEquals("Car(model='M3', brand=BMW, maxSpeed=260, hp=80, torque=160, seating=2, mileage=14)", car.toString())
+  }
+
 }
