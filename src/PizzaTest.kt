404bd3fd42037aec0b88f085dd16391cc2c264ef
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-16 00:06:16 -0500

    Added Avoid creating unnecessary objects (Effective Kotlin Item 5)

---
+++ src/PizzaTest.kt
@@ -0,0 +1,21 @@
+import org.junit.After
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Naren on 6/15/17.
+ */
+class PizzaTest {
+  @After fun tearDown() {}
+
+  @Test fun getPizzaMaker() {}
+
+  @Test fun setPizzaMaker() {}
+
+  @Test fun makePizza() {}
+
+  @Test fun getSize() {}
+
+  @Test fun getToppings() {}
+}
