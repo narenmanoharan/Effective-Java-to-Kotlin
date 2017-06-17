af9ef7b79544cddcb9cada3dcfef9365a15aded8
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-17 11:25:35 -0500

    Added Implementing Comparable and Using ComparatorsImplementing Comparable and Using Comparators (Effective Kotlin Item 12)

---
+++ src/main/Movie.kt
@@ -0,0 +1,20 @@
+package main
+
+/**
+ * Created by Naren on 6/17/17.
+ */
+
+
+data class Movie(val name: String, val rating: Double, val year: Int) : Comparable<Movie> {
+
+  override fun compareTo(other: Movie): Int {
+    return this.rating.compareTo(other.rating)
+  }
+
+}
+
+
+
+
+
+
---
+++ src/test/MovieTest.kt
@@ -0,0 +1,18 @@
+package main
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Naren on 6/17/17.
+ */
+class MovieTest {
+  @Test fun compareTo() {}
+
+  @Test fun getName() {}
+
+  @Test fun getRating() {}
+
+  @Test fun getYear() {}
+}
