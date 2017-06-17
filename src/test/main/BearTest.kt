9a9a90b7c12b9979ba06ab3a3b4cf645c7a71944
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-17 13:19:39 -0500

    Moved test files to Main folder and Added Effective Kotlin Item 13 test

---
+++ src/test/main/BearTest.kt
@@ -0,0 +1,23 @@
+import main.Bear
+import main.Food.Fish
+import main.Food.Shrimp
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+/**
+ * Created by Naren on 6/17/17.
+ */
+class BearTest {
+
+  val bear: Bear = Bear(age = 2 , food = listOf(Fish, Shrimp))
+
+  @Test fun getFood() {
+    assertEquals(listOf(Fish, Shrimp), bear.food)
+  }
+
+  @Test fun copy() {
+    val kidBear: Bear = bear.copy()
+    assertEquals(kidBear, bear)
+  }
+}
