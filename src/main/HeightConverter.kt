1c87c349c9df2de89529fc3ea4ab5fa4f3d07db7
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-19 16:38:53 -0500

    Added Accessor Methods/Properties (Effective Kotlin item 14)

---
+++ src/main/HeightConverter.kt
@@ -0,0 +1,27 @@
+package main
+
+import main.Units.cm
+import main.Units.m
+
+/**
+ * Created by Naren on 6/19/17.
+ */
+
+class HeightConverter {
+
+  var value: Int = 1
+
+  var from: Units = cm
+
+  var to: Units = m
+
+  private var convert : Int = value * 100
+
+  var newValue: Int = this.convert
+    private set
+
+}
+
+enum class Units {
+  cm, m
+}
---
+++ src/test/main/HeightConverterTest.kt
@@ -0,0 +1,22 @@
+package main
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Naren on 6/19/17.
+ */
+class HeightConverterTest {
+  @Test fun getValue() {}
+
+  @Test fun setValue() {}
+
+  @Test fun getFrom() {}
+
+  @Test fun setFrom() {}
+
+  @Test fun getTo() {}
+
+  @Test fun setTo() {}
+}
