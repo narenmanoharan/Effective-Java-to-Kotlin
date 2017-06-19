326ec05d7132928f1b753681072020f30973b387
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-19 16:39:13 -0500

    Added Effective Kotlin item 14 test

--- src/test/main/HeightConverterTest.kt
+++ src/test/main/HeightConverterTest.kt
@@ -1,22 +1,56 @@
 package main
 
+import main.Units.cm
+import main.Units.m
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Created by Naren on 6/19/17.
  */
 class HeightConverterTest {
-  @Test fun getValue() {}
 
-  @Test fun setValue() {}
+  var converter: HeightConverter? = null
 
-  @Test fun getFrom() {}
+  @Before fun setUp() {
+    converter = HeightConverter()
+    converter?.value = 200
+    converter?.from = cm
+    converter?.to = m
+  }
 
-  @Test fun setFrom() {}
+  @Test fun setValue() {
+    converter?.value = 200
+    assertEquals(200, converter?.value)
+  }
 
-  @Test fun getTo() {}
+  @Test fun getValue() {
+    assertEquals(200, converter?.value)
+  }
 
-  @Test fun setTo() {}
+  @Test fun setFrom() {
+    converter?.from = m
+    assertEquals(Units.m, converter?.from)
+  }
+
+  @Test fun getFrom() {
+    assertEquals(Units.cm, converter?.from)
+  }
+
+  @Test fun setTo() {
+    converter?.to = cm
+    assertEquals(Units.cm, converter?.to)
+  }
+
+  @Test fun getTo() {
+    assertEquals(Units.m, converter?.to)
+  }
+
+  @Test fun getNewValue() {
+    assertEquals(100, converter?.newValue)
+  }
+
+
 }
