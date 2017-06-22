8ce909349e72bdea1aab4cdb69340c1033708df0
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-23 02:38:50 +0530

    Updated sheep class

--- src/main/kotlin/Sheep.kt
+++ src/main/kotlin/Sheep.kt
@@ -12,6 +12,25 @@ class Sheep(val name: String, val age: Int) : Cloneable {
     return super.clone() as main.Sheep
   }
 
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other?.javaClass != javaClass) return false
+
+    other as Sheep
+
+    if (name != other.name) return false
+    if (age != other.age) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = name.hashCode()
+    result = 31 * result + age
+    return result
+  }
+
+
 }
 
 // Using the data class with copy (Kotlin way)
--- src/test/kotlin/SheepTest.kt
+++ src/test/kotlin/SheepTest.kt
@@ -6,6 +6,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
+import com.google.common.truth.Truth.assertThat
+
 /**
  * Created by Naren on 6/16/17.
  */
@@ -21,6 +23,31 @@ class SheepTest {
     val evilSheep: Sheep = sheep.clone() as main.Sheep
     assertEquals(sheep.name, evilSheep.name)
     assertEquals(sheep.age, evilSheep.age)
+  }
+
+  @Test fun trueClone() {
+    assertThat(sheep.clone())
+        .isEqualTo(sheep)
+  }
+
+  @Test fun testSheepEquals() {
+    assertThat(sheep.clone())
+        .isEqualTo(sheep)
+  }
+
+  @Test fun testSheepHashCode() {
+    assertThat(sheep.hashCode())
+        .isEqualTo(1992252427)
+  }
+
+  @Test fun getSheepName() {
+    assertThat(sheep.name)
+        .isEqualTo("Black")
+  }
+
+  @Test fun getSheepAge() {
+    assertThat(sheep.age)
+        .isEqualTo(10)
   }
 
   @Test fun getName() {
