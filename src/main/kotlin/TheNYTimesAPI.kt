4a0b85add1a5787ae031e9256f6563ca75cd859c
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-23 03:57:12 +0530

    Use interface only to define types (Effective Kotlin item 19)

---
+++ src/main/kotlin/TheNYTimesAPI.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by Naren on 6/23/17.
+ */
+
+class TheNYTimesAPI {
+  companion object {
+    const val API_KEY = "bfa504d8afec47ba9757b3dab9201ddd"
+    const val BASE_URL = "https://api.nytimes.com/"
+    const val API_ENDPOINT = "/svc/search/v2/articlesearch.json"
+  }
+}
