159b221b8467e52df875416098d62593bd31dd1e
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-26 09:50:48 +0530

    Don't use raw types in code (Effective Kotlin item 23)

---
+++ src/main/kotlin/Box.kt
@@ -0,0 +1,38 @@
+/**
+ * Created by Naren on 6/26/17.
+ */
+
+class Box<T>(private val name: String, private val items: List<T>) {
+
+  fun getItem(): List<T> {
+    return items
+  }
+
+  fun getName(): String {
+    return name
+  }
+}
+
+class BoxOut<out T>(private val name: String, private val items: List<T>) {
+
+  fun getItem(): List<T> {
+    return items
+  }
+
+  fun getName(): String {
+    return name
+  }
+
+}
+
+
+class BoxIn<in T> {
+
+  fun toString(value: T): String {
+    return value.toString()
+  }
+}
+
+enum class Books {
+  Java, Kotlin, Android, Testing, Mockito, JUnit
+}
