79c197ba23d347ed7a82af1e49d2d5036f081cbe
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-06-26 23:16:18 +0530

    Added stack

--- src/main/kotlin/Stack.kt
+++ src/main/kotlin/Stack.kt
@@ -1,6 +1,35 @@
+import java.util.Arrays
+import java.util.EmptyStackException
+
 /**
  * Created by Naren on 6/26/17.
  */
 
 
-class Stack
+class Stack(var elements: Array<Any>?, private var size: Int = 0, private val capacity: Int = 16) {
+
+  fun push(e: Any) {
+    ensureCapacity()
+    elements?.set(size++, e)
+  }
+
+  fun pop(): Any {
+    if (size == 0)
+      throw EmptyStackException()
+    val result: Any = elements!![--size]
+    return result
+  }
+
+  fun isEmpty(): Boolean {
+    return size == 0
+  }
+
+  private fun ensureCapacity() {
+    if (elements?.size == size)
+      elements = Arrays.copyOf(elements!!, 2 * size + 1)
+  }
+
+  override fun toString(): String {
+    return "Stack(elements=${Arrays.toString(elements)}, size=$size, capacity=$capacity)"
+  }
+}
